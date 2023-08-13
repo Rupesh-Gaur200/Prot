8c4b711f3d0f6d14a4de39b4522da66c96ccfc6b
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-08-13 22:59:02 +0530

    NavBar-Component

---
+++ Protfolio/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+function Navbar(){
+    return(
+        <div div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+          
+        </div>
+    )
+}
+export default Navbar;
